5452b08634e0d859d8695d3b06737c1fce714958
EthanDevelops <66741050+EthanDevelops@users.noreply.github.com>
2021-12-03 02:13:30 +0000

    HAI

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/* yoinked from https://github.com/bybilly/minecraft-web-portal/blob/main/js/main.js */
+const doParticles = true;
+
+const getWidth = () => {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+};
+
+if (doParticles) {
+    if (getWidth() < 400) $.firefly({
+        minPixel: 1,
+        maxPixel: 2,
+        total: 20
+    });
+    else $.firefly({
+        minPixel: 1,
+        maxPixel: 3,
+        total: 40
+    });
+}
+
+$(document).ready(() => {
+    let ip = $(".sip").attr("data-ip");
+    let port = $(".sip").attr("data-port");
+    if (port == "" || port == null) port = "25565";
+    if (ip == "" || ip == null) return console.error("Error fetching player count - is the IP set correctly in the HTML?");
+    updatePlayercount(ip, port);
+    setInterval(() => {
+        updatePlayercount(ip, port);
+    }, 60000);
+});
+
+const updatePlayercount = (ip, port) => {
+    $.get(`https://api.bybilly.uk/api/players/${ip}/${port}`, (result) => {
+        if (result.hasOwnProperty('online')) {
+            $(".sip").html(result.online);
+        } else {
+            $(".playercount").html("Server isn't online!");
+        }
+    });
+};
